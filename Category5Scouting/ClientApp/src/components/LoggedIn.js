98c7e514ab0dc91aa691018bce659c112519ba41
Connor <61102529+Metater@users.noreply.github.com>
2022-11-19 11:38:16 -0500

    Update a ton of stuff, the template updated to a new react version and stuff, it was painful

--- Category5Scouting/ClientApp/src/components/LoggedIn.js
+++ Category5Scouting/ClientApp/src/components/LoggedIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Routes, Route } from "react-router-dom";
 
 import { Clicker } from './Clicker';
 import { Home } from './Home';
@@ -9,23 +9,46 @@ import { Settings } from './Settings';
 import { UsefulLinks } from './UsefulLinks';
 import { WeatherForecast } from './WeatherForecast';
 
+const GetRoutes = (state) => [
+  {
+    index: true,
+    element: <Home />
+  },
+  {
+    path: "/weather-forecast",
+    element: <WeatherForecast />
+  },
+  {
+    path: "/useful-links",
+    element: <UsefulLinks />
+  },
+  {
+    path: "/scout",
+    element: <Scout />
+  },
+  {
+    path: "/clicker",
+    element: <Clicker state={state} />
+  },
+  {
+    path: "/settings",
+    element: <Settings state={state} />
+  }
+];
+
 export function LoggedIn( {state} ) {
   return (
     <>
       {/* Always render the menu at the top */}
       <Menu />
-
+      
       {/* Render the appropriate components based on the url */}
-      <Route exact path="/" component={Home} />
-      <Route path="/weather-forecast" component={WeatherForecast} />
-      <Route path="/useful-links" component={UsefulLinks} />
-      <Route path="/scout" component={Scout} />
-      <Route path="/clicker">
-        <Clicker state={state} />
-      </Route>
-      <Route path="/settings">
-        <Settings state={state} />
-      </Route>
+      <Routes>
+        {GetRoutes(state).map((route, index) => {
+          const { element, ...rest } = route;
+          return <Route key={index} {...rest} element={element} />;
+        })}
+      </Routes>
     </>
   );
 };
--- Category5Scouting/ClientApp/src/setupProxy.js
+++ Category5Scouting/ClientApp/src/setupProxy.js
@@ -1,17 +1,20 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:42837';
 
 const context =  [
-    "/api"
+  "/api",
 ];
 
 module.exports = function(app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
-    secure: false
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
   });
 
   app.use(appProxy);
