83702d1d8ebf19539cf7029faf3c728fb882c304
Ben S <beansiegal@gmail.com>
2023-03-04 23:26:06 -0500

    Emojis changed to 10 numbers

--- Category5Scouting/ClientApp/src/components/elements/SyncedEmojiSelect.js
+++ Category5Scouting/ClientApp/src/components/elements/SyncedEmojiSelect.js
@@ -10,11 +10,16 @@ export const SyncedEmojiSelect = () => {
   const [radioValue, setRadioValue] = useState(0);
 
   const radios = [
-    { name: "😭", value: -2 },
-    { name: "😢", value: -1 },
-    { name: "😐", value: 0 },
-    { name: "😁", value: 1 },
-    { name: "😍", value: 2 },
+    { name: "1", value: 1 },
+    { name: "2", value: 2 },
+    { name: "3", value: 3 },
+    { name: "4", value: 4 },
+    { name: "5", value: 5 },
+    { name: "6", value: 6 },
+    { name: "7", value: 7 },
+    { name: "8", value: 8 },
+    { name: "9", value: 9 },
+    { name: "10", value: 10 },
   ];
 
   let getVariant = (value) => {
