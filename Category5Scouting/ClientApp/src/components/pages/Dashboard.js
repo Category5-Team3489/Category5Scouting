1c1f28f8b38016525dde1bb93a9668541ca3ecee
Ben S <beansiegal@gmail.com>
2023-03-05 23:57:08 -0500

    Dashboard styling

--- Category5Scouting/ClientApp/src/components/pages/Dashboard.js
+++ Category5Scouting/ClientApp/src/components/pages/Dashboard.js
@@ -3,7 +3,13 @@ import React, { useEffect, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
 
-export const Dashboard = ( {state} ) => {
+import Accordion from 'react-bootstrap/Accordion';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { SyncedTextBox } from '../elements/SyncedTextBox';
+
+export const Dashboard = ({selectedTeamState}) => {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
@@ -13,25 +19,66 @@ export const Dashboard = ( {state} ) => {
   }, []);
 
   return (
-    <Container className="p-4">
-      <Alert>
+    <div>
+      <Alert className="m-4">
         <Alert.Heading>
           Dashboard
         </Alert.Heading>
-        {teams.map((team, idx) =>
-          <Alert key={idx}>
-            {team.nickname}
-            <br />
-            {team.team_number}
-            <br />
-            {team.city}
+      </Alert>
+      {teams.map((team, idx) =>
+        <Container className="p-4">
+          <Row>
+            <Col>
+              <div class="card bg-light">
+                <div class="card-body" key={idx}>
+                  <h4 class="card-header">
+                    <b>{team.nickname}</b>
+                    <br />
+                    {team.team_number}
+                    <br />
+                  </h4>
+                  <Accordion>
+                    <Accordion.Header><h6>Auto</h6></Accordion.Header>
+                    <Accordion.Body>
+                      <li class="list-group-item">Grippiness: </li>
+                      <li class="list-group-item">Placement: </li>
+                      <li class="list-group-item">Navigability: </li>
+                    </Accordion.Body>
+                  </Accordion>
+                  <div class="dropdown-divider"></div>
+                  <Accordion>
+                    <Accordion.Header><h6>Tele-op</h6></Accordion.Header>
+                    <Accordion.Body>
+                      <li class="list-group-item">Grippiness: </li>
+                      <li class="list-group-item">Placement: </li>
+                      <li class="list-group-item">Elusive Driving: </li>
+                      <li class="list-group-item">Defensive Driving: </li>
+                      <li class="list-group-item">Drive Team Coordination: </li>
+                      <li class="list-group-item">Charge Station Balancing: </li>
+                    </Accordion.Body>
+                  </Accordion>
+                  <div class="dropdown-divider"></div>
+                  <Accordion>
+                    <Accordion.Header><h6>Strategy Notes</h6></Accordion.Header>
+                    <Accordion.Body>
+                      <SyncedTextBox
+                        placeholder="Overview of team"
+                        rows={3}
+                        name={team.team_number}
+                      />
+                    </Accordion.Body>
+                  </Accordion>
+                  {/* {team.city}
             <br />
             {team.state_prov}
             <br />
-            {team.country}
-          </Alert>
-        )}
-      </Alert>
-    </Container>
+            {team.country} */}
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </div>
   );
 };
