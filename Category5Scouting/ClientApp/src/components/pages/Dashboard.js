e26ad92f84f8ab751e429e887f8993a0a8019ddf
Ben S <beansiegal@gmail.com>
2023-03-07 10:44:44 -0500

    Update Dashboard.js

--- Category5Scouting/ClientApp/src/components/pages/Dashboard.js
+++ Category5Scouting/ClientApp/src/components/pages/Dashboard.js
@@ -9,7 +9,7 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import { SyncedTextBox } from '../elements/SyncedTextBox';
 
-export const Dashboard = ({selectedTeamState}) => {
+export const Dashboard = ({ selectedTeamState }) => {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
@@ -40,21 +40,21 @@ export const Dashboard = ({selectedTeamState}) => {
                   <Accordion>
                     <Accordion.Header><h6>Auto</h6></Accordion.Header>
                     <Accordion.Body>
-                      <li class="list-group-item">Grippiness: 
-                      <SyncedTextBox
-                  placeholder="Overview of team"
-                  rows={3}
-                  name={team.team_number + "Auto Grippiness"}
-                />
-                </li>
-                      <li class="list-group-item">Placement: 
-                      <SyncedTextBox
-                  placeholder="Overview of team"
-                  rows={3}
-                  name={team.team_number + "Auto Placement"}
-                />
-                
-                </li>
+                      <li class="list-group-item">Grippiness:
+                        <SyncedTextBox
+                          placeholder="Overview of team"
+                          rows={3}
+                          name={team.team_number + "Auto Grippiness"}
+                        />
+                      </li>
+                      <li class="list-group-item">Placement:
+                        <SyncedTextBox
+                          placeholder="Overview of team"
+                          rows={3}
+                          name={team.team_number + "Auto Placement"}
+                        />
+
+                      </li>
                       <li class="list-group-item">Navigability: </li>
                     </Accordion.Body>
                   </Accordion>
@@ -72,10 +72,15 @@ export const Dashboard = ({selectedTeamState}) => {
                   </Accordion>
                   <div class="dropdown-divider"></div>
                   <Accordion>
-                    <Accordion.Header><h6>Strategy Notes</h6></Accordion.Header>
+                    <Accordion.Header><h6>Additional and Strategy Notes</h6></Accordion.Header>
                     <Accordion.Body>
                       <SyncedTextBox
-                        placeholder="Overview of team"
+                        placeholder="Additional Notes"
+                        rows={3}
+                        name={selectedTeamState.get().team_number + "Additional Notes:"}
+                      />
+                      <SyncedTextBox
+                        placeholder="Strategy Notes"
                         rows={3}
                         name={team.team_number}
                       />
@@ -85,7 +90,7 @@ export const Dashboard = ({selectedTeamState}) => {
                   <Accordion>
                     <Accordion.Header><h6>DNP?</h6></Accordion.Header>
                     <Accordion.Body>
-                      
+
                     </Accordion.Body>
                   </Accordion>
                   {/* {team.city}
