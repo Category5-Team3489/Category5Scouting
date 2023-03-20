1de7b391020f5293ab42faf279a93b6a6fffceee
Ben S <beansiegal@gmail.com>
2023-03-19 20:14:13 -0400

    Two scouters' ranks

--- Category5Scouting/ClientApp/src/components/elements/TeamOverview.js
+++ Category5Scouting/ClientApp/src/components/elements/TeamOverview.js
@@ -2,6 +2,10 @@ import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
 import { SyncedTextBox } from './SyncedTextBox';
 import { SyncedNumberSelect } from '../elements/SyncedNumberSelect';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 
 
 export const TeamOverview = ( {selectedTeamState} ) => {
@@ -20,6 +24,11 @@ export const TeamOverview = ( {selectedTeamState} ) => {
 <Alert.Heading>
   Overall Team Rank
 </Alert.Heading>
+{/* <Container fluid> */}
+<b>Scouter 1 Ranking:</b>
+<SyncedNumberSelect name={selectedTeamState.get().team_number + "Overall Rank"} />
+<br />
+<b>Scouter 2 Ranking:</b>
 <SyncedNumberSelect name={selectedTeamState.get().team_number + "Overall Rank"} />
     </Alert>
   );
